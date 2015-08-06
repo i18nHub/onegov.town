599e2ee6a9ca1f01762764227efbe0bb18b73c8a
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-06 13:03:48 +0200

    Adds a clipboard model

---
+++ onegov/town/models/clipboard.py
@@ -0,0 +1,28 @@
+from cached_property import cached_property
+
+
+class Clipboard(object):
+    """ The clipboard holds a url that should be copied and then pasted.
+    The url is expected to be stored in a token that has been created by
+    :meth:`onegov.core.request.CoreRequest.new_url_safe_token`.
+
+    The reason behind this is that the url is used to fetch the object behind
+    the url in an unrestricted fashion.
+
+    """
+
+    def __init__(self, request, token):
+        self.request = request
+        self.token = token
+
+    @cached_property
+    def url(self):
+        data = self.request.load_url_safe_token(self.token, salt='clipboard')
+        return data and data['url'] or None
+
+    @classmethod
+    def from_url(cls, request, url):
+        return cls(
+            request,
+            request.new_url_safe_token({'url': url}, salt='clipboard')
+        )
